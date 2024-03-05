7ca2921cd7a9330176bf985a171df447f6557575
Yahir Alberto <136031489+yahirdev13@users.noreply.github.com>
2024-03-04 19:27:05 -0600

    cambios de las cards

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,54 +8,121 @@
       <b-button variant="primary" class="ms-3 me-3" size="lg">mostrar si tiene imagen</b-button>
     </div>
 
-    <div class="cards ms-3 mt-5 me-3">
-      <b-card img-src="https://picsum.photos/600/300/?image=25" img-alt="Image" img-top tag="article"
-        style="max-width: 20rem;" class="mb-2">
-        <b-card-text>
-          Aqui va descripción random del libro
-        </b-card-text>
-      </b-card>
+    <div class="libros">
+
+      <div class="cards ms-3 mt-5 me-3" v-for="(libro, index) in libros" :key="index">
+        <b-card :img-src="libro.imagen" img-alt="Image" img-top tag="article" style="max-width: 20rem;" class="mb-2">
+          <b-card-text>
+            <b>Autor:</b> {{ libro.autor }}
+          </b-card-text>
+          <b-card-text>
+            <b>Titulo:</b> {{ libro.titulo }}
+          </b-card-text>
+          <b-card-text>
+            <b>Fecha de publicacion:</b> {{ libro.fecha }}
+          </b-card-text>
+        </b-card>
+      </div>
+
+      <div class="crud">
+
+        <div class="mt-5">
+          <b-button variant="outline-success" @click="showForm = true">
+            <img src="../assets/plus.png" />
+          </b-button>
+        </div>
+        <div class="mt-5">
+          <b-button variant="outline-warning">
+            <img src="../assets/pencil.png" />
+          </b-button>
+        </div>
+        <div class="mt-5">
+          <b-button variant="outline-danger">
+            <img src="../assets/trash.png" />
+          </b-button>
+        </div>
+      </div>
     </div>
 
-    <div class="crud">
+    <!-- Formulario -->
+    <b-modal v-model="showForm" title="Agregar Libro">
+      <form @submit.prevent="submitForm">
+        <b-form-group id="titulo" label="Título:" label-for="titulo-input">
+          <b-form-input id="titulo-input" v-model="titulo" required></b-form-input>
+        </b-form-group>
 
-      <div class="mt-5">
-        <b-button variant="outline-success">
-          <img src="../assets/plus.png" />
-        </b-button>
-      </div>
-      <div class="mt-5">
-        <b-button variant="outline-warning">
-          <img src="../assets/pencil.png" />
-        </b-button>
-      </div>
-      <div class="mt-5">
-        <b-button variant="outline-danger">
-          <img src="../assets/trash.png" />
-        </b-button>
-      </div>
-    </div>
+        <b-form-group id="autor" label="Autor:" label-for="autor-input">
+          <b-form-input id="autor-input" v-model="autor" required></b-form-input>
+        </b-form-group>
+
+        <b-form-group id="fecha" label="Fecha de Publicación:" label-for="fecha-input">
+          <b-form-input id="fecha-input" type="date" v-model="fecha" required></b-form-input>
+        </b-form-group>
+
+        <b-button type="submit" variant="primary">Agregar</b-button>
+        <b-button variant="danger" @click="cancelForm">Cancelar</b-button>
+      </form>
+    </b-modal>
+
 
   </div>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      showForm: false,
+      titulo: '',
+      autor: '',
+      fecha: '',
+      libros: [
+        { titulo: 'Libro 1', autor: 'Autor 1', fecha: '2023-01-01', imagen: 'https://picsum.photos/600/300/?image=25' },
+        { titulo: 'Libro 2', autor: 'Autor 2', fecha: '2022-05-15', imagen: 'https://picsum.photos/600/300/?image=26' },
+        { titulo: 'Libro 3', autor: 'Autor 3', fecha: '2021-11-30', imagen: 'https://picsum.photos/600/300/?image=27' }
+      ]
+    };
+  },
+  methods: {
+    submitForm() {
+      // Aquí puedes enviar los datos a tu backend o hacer lo que necesites con ellos
+      console.log("Datos del formulario:", this.titulo, this.autor, this.fecha);
+      // Limpia los campos del formulario
+      this.titulo = '';
+      this.autor = '';
+      this.fecha = '';
+      // Oculta el formulario después de enviar
+      this.showForm = false;
+    },
+    cancelForm() {
+      // Limpia los campos del formulario si se cancela
+      this.titulo = '';
+      this.autor = '';
+      this.fecha = '';
+      // Oculta el formulario
+      this.showForm = false;
+    }
+  }
+};
 </script>
 
 <style scoped>
-.container {
+.libros {
   display: flex;
-  justify-content: flex-end;
-  margin-left: auto;
-  /* Esto empujará el contenedor hacia la derecha */
-  width: calc(100% - 200px);
-  /* Establece el ancho menos el ancho del contenedor "crud" */
+  /* División de la clase libros en dos columnas */
+  flex-wrap: wrap;
+}
+
+.cards {
+  flex: 1;
+  /* La primera columna (cards) toma todo el espacio disponible */
 }
 
 .crud {
   width: 200px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
 }
 </style>
